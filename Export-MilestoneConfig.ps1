48bcf6bd67fd23c5b11520ea13af664d3f7d9dfd
James Preston <jamespreston@outlook.com>
2017-08-14 16:18:13 +0100

    v1

---
+++ Export-MilestoneConfig.ps1
@@ -0,0 +1,10 @@
+﻿[xml]$data = Get-Content "C:\ProgramData\Milestone\Milestone Surveillance\configuration.xml"
+
+$data.config.Devices.ChildNodes | Export-Csv C:\temp\devices.csv -NoTypeInformation
+
+$cameras = foreach($device in $data.config.Devices.ChildNodes){
+    $device.Cameras.ChildNodes
+    $device.Cameras.ChildNodes | Add-Member -MemberType NoteProperty -Name 'Parent Device' -Value $device.DisplayName
+}
+
+$cameras | Export-Csv C:\temp\cameras.csv -NoTypeInformation
